76abddba9bf1811eb0a14577ccb0fc7e7c228a5e
lesley <lesleybanadzem2017@gmail.com>
2019-12-06 15:48:10 -0800

    Wrote down the endpoint for getting a specific post and hadling errors with regards to wrong ids and not providing an id

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -8,7 +8,6 @@ const db = require('./../data/db');
 router.get('/api/post', (req, res)=>{
     db.find()
         .then(response => {
-            console.log(response)
             res.status(200).send({
                 message: 'Get successful',
                 post: response
@@ -16,6 +15,25 @@ router.get('/api/post', (req, res)=>{
         })
 })
 
+// get specific post
+router.get('/api/post/:id', (req, res)=>{
+    const id = req.params.id;
 
+    if(!id){
+        res.status(404).send({message: `Error, You didn't provide an id for the post request`})
+    } else{
+        db.findById(id)
+        .then(response => {
+            if(response.length < 1){
+                res.status(404).send({message: "The post with the specified ID does not exist."})
+            } else{
+                res.status(200).send({
+                    message: 'Get successful',
+                    post: response
+                });
+            }
+        }).catch(err => res.status(500).send({message: "The post information could not be retrieved." }))
+    }
+})
 
 module.exports = router;
