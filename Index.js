49d042836d118dbe5d429117886b42f70632a740
lesley <lesleybanadzem2017@gmail.com>
2019-12-06 16:20:31 -0800

    Endpoint for posting data

--- Index.js
+++ Index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 
 const PORT = 8080;
-
 const app = express();
-
-//Routers
-const postRouters = require("./routers/postRouter");
-
-app.use('/', postRouters)
 
 
 app.use(express.json());
+//Routers
+const postRouters = require("./routers/postRouter");
+
+app.use('/api/post', postRouters)
+
+
 
 app.get('/', (req, res)=>{
     res.status(200).send(`<h1> Welcome to Node-Api2, Enjoy the endpoints</h1>`)
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const db = require('./../data/db');
 
 //Get endpoint
-router.get('/api/post', (req, res)=>{
+router.get('/', (req, res)=>{
     db.find()
         .then(response => {
             res.status(200).send({
@@ -16,24 +16,44 @@ router.get('/api/post', (req, res)=>{
 })
 
 // get specific post
-router.get('/api/post/:id', (req, res)=>{
+router.get('/:id', (req, res)=>{
     const id = req.params.id;
 
     if(!id){
         res.status(404).send({message: `Error, You didn't provide an id for the post request`})
     } else{
         db.findById(id)
-        .then(response => {
-            if(response.length < 1){
-                res.status(404).send({message: "The post with the specified ID does not exist."})
-            } else{
-                res.status(200).send({
-                    message: 'Get successful',
-                    post: response
-                });
-            }
-        }).catch(err => res.status(500).send({message: "The post information could not be retrieved." }))
+            .then(response => {
+                if(response.length < 1){
+                    res.status(404).send({message: "The post with the specified ID does not exist."})
+                } else{
+                    res.status(200).send({
+                        message: 'Get successful',
+                        post: response
+                    });
+                }
+            })
+            .catch(err => res.status(500).send({message: "The post information could not be retrieved." }))
     }
 })
 
+//Post to the Db
+router.post('/', (req, res)=>{
+
+    const { title, contents } =req.body
+    if(!title || !contents){
+        res.status(400).json({Error: 'Please Provide title and contents'});
+    }else{
+        db.insert({title, contents})
+            .then(response => {
+                res.status(201).send({
+                    message: 'Post successful',
+                    post: response
+                });
+            })
+            .catch(err =>{
+                res.status(500).json({error: "There was an error while saving the post to the database"})
+            })
+    }
+})
 module.exports = router;
